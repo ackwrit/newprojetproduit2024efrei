3b131f6f8c44d85a3847eceaac42dad6e56fddc8
skweel <dissingardjino@icloud.com>
2024-11-06 09:35:26 +0100

    mise en place à jour 9:35

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:amazonmini/view/my_background.dart';
+import 'package:amazonmini/view/my_login_page.dart';
+import 'package:amazonmini/view/my_register_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -86,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed:(){
                 setState((){
                   counter = 0;
-                })
+                });
               }
           ),
         ]
